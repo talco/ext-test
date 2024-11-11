009e7d5938018da37e26f31a16f08d8ea07ee55f
Tal Cohen <tal7814@gmail.com>
2024-11-11 13:38:12 +0200

    wip

---
+++ .web-ext-config.mjs
@@ -0,0 +1,15 @@
+export default {
+//   version: "0.0.1",
+//   verbose: true,
+  // Command options:
+  build: {
+    overwriteDest: true,
+  },
+  sign: {
+    apiKey: "user:18668519:407",
+    apiSecret: "0a92fe8dbe5c19a52b6b040febe3fdbc07378098debe48b54d385c631d54cda8",
+  },
+  sourceDir: 'src',
+  artifactsDir: 'artifact',
+  ignoreFiles: ["package-lock.json", "package.lock"],
+};
